519694357b5ce9fefe9bda337982eff8861208f8
Thalita <119790076+FernandesThalita@users.noreply.github.com>
2023-07-25 22:37:41 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,16 +7,28 @@ const local_nao_encontrado = document.querySelector('.local-nao-encontrado');
 
 
 //Funcao click para pesquisa
-pesquisaButton.addEventListener("click", ()=>{
+pesquisaButton.addEventListener("click", () => {
 
     //chave API
     const APIKey = 'f4f46e638046e2d629d7bae581a1da6a';
     //valor do input(cidade ou local pesquisado)
     const cidadePesquisada = document.querySelector('.pesquisa_barra input').value;
 
-    if(cidadePesquisada ===''){
+    //if para caso o usuario não preencha o campo
+    if (cidadePesquisada === '') {
         alert('preencha o campo antes de pesquisar!');
         return;
     }
+
+    //fetch para obter a URL da API
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidadePesquisada}&units=metric&appid=${APIKey}`).then(resposta => resposta.json()).then(data =>{
+
+    //Verificando se o code da resposta da API é 404 (não encontrado)
+        if(data.code === '404'){
+            //style para a tag de não encontrado
+            container.style.height = '400px';
+            box_clima
+        }
+    })
 
 })
